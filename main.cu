d0e1c8af8798cf87894661d5e08439b04ecf8928
Dejan Peretin <dperetin@gmail.com>
2011-03-14 09:46:50 +0100

    Trazenje inverza i mnozenje prebaceno u rjesavanje sustava.
    Traba testirati.

--- main.cu
+++ main.cu
@@ -6,6 +6,51 @@
 
 using namespace std;
 
+void cpu_dpotrf(float *m_in, float *m_out, int size)
+{
+	    for (int i = 0; i < size; i++) {
+			        float sum = 0;
+					        for (int k = 0; k < i; k++){
+								            sum += (m_out[k * size + i] * m_out[k * size + i]);
+											        }
+							        m_out[i * size + i] = sqrt(m_in[i * size + i] - sum);
+									        for (int j = i + 1; j < size; j++ ) {
+												            sum = 0;
+															            for (int k = 0; k < i; k++){
+																			                sum += (m_out[k * size + i] * m_out[k * size + j]);
+																							            }
+																		            m_out[i * size + j] = (m_in[i * size + j] - sum) / m_out[i * size + i];
+																					        }
+											    }
+}
+
+
+
+void standard (float *A, float  *B, float *C, int size)
+{
+	    int i, j, k;
+
+		    for (i = 0; i < size; i++)
+				        for (j = 0; j < size; j++)
+							            for (k = 0; k < size; k++)
+
+										{
+
+											                C[i * size + j] += A[k * size + i] * B[k * size + j]; 
+															if (i==j)C[i*size+j]+= 0.001;
+										}
+}
+
+void init(float *v, int n)
+{
+	    int i;
+		    srand(time(NULL));
+			    for (i = 0; i < n; i++)
+					        v[i] = rand() / (float(RAND_MAX) + 1)-1; 
+}
+
+
+
 void loadMatrix(float * matrix, char *s, int size)
 {
 	fstream f;
@@ -44,13 +89,11 @@ __global__ void gpu_dpotrf(float *m, int size, int p)
 
     float fac;
 
-
-// in this loop tx labels column, ty row
-#pragma unroll 16
+	#pragma unroll 16
     for (int k = 0; k < 16; k++)
     {
 		__syncthreads();
-		fac = rsqrtf(a[k][k]);
+		fac = rsqrt(a[k][k]);
 		__syncthreads();
 		if ((ty == k) && (tx >= k)) 
 	    	a[tx][ty] = (a[tx][ty]) * fac;
@@ -65,8 +108,6 @@ __global__ void gpu_dpotrf(float *m, int size, int p)
 
     __syncthreads();
 
-
-// here, tx labels column, ty row	
     if (ty>=tx) 
 	m[(tx+16*p)*size+ty+16*p]=a[ty][tx];
     
@@ -74,22 +115,29 @@ __global__ void gpu_dpotrf(float *m, int size, int p)
 
 }
 
-__global__ void gpu_inv_l(float *u, float *b, int size, int p)
+__global__ void gpu_inv_l(float *u, int size, int p)
 {
 	int i, j;
+
 	int tid = threadIdx.x;
-	b[0 * 16 + tid] = b[0 * 16 + tid] / 
+	int bx=blockIdx.x+1;
+	__shared__ float b[16][16];
+	for(i=0;i<16;i++)
+		b[i][tid]=u[(i+p*16)*size+tid+(bx+p)*16];
+	b[0][tid] = b[0][tid] / 
 		u[(0 + p * 16) * size + (0 + 16 * p)];
 	for (i = 1; i < 16; i++){
 		for (j = 0; j < i; j++){
-			b[i * 16 + tid] = b[i * 16 + tid] - 
+			b[i][tid] = b[i][tid] - 
 				u[(j + p * 16) * size + (i + p * 16)] *
-				b[j * 16 + tid];
+				b[j][tid];
 		}
-		b[i * 16 + tid] = b[i * 16 + tid] / u[(i + p * 16) * size + 
+		b[i][tid] = b[i][tid] / u[(i + p * 16) * size + 
 							(i + 16 *p)];
 
 	}
+	for(i=0;i<16;i++)
+		u[(i+p*16)*size+tid+(bx+p)*16]=b[i][tid];
 }
 
 __global__ void gpu_mm_a(float *m, int size, int p, int it)
@@ -154,14 +202,18 @@ void init_eye(float *v, int n)
 int main(int argc, char *argv[])
 {
 	int size = 1024;
-	unsigned int timer2 = 0, t = 0, t2 = 0;
+	unsigned int timer2 = 0, t = 0, t2 = 0, ta = 0;
 
-	float *m_in, *m_out, *device_m, *device_m_out, *eye, *device_eye;
+	float *m_in, *m_out, *device_m, *device_m_out, *eye, *device_eye, *v, *cpu_rez;
 	m_in = new float[size * size];
 	m_out = new float[size * size];
+	cpu_rez = new float[size * size];
+	v = new float[size * size];
 	eye = new float[16 * 16];
 
+	memset(m_in, 0, size * size * sizeof(float));
 	memset(m_out, 0, size * size * sizeof(float));
+	memset(cpu_rez, 0, size * size * sizeof(float));
 	memset(eye, 0, 16 * 16 * sizeof(float));
 
 	init_eye(eye, 16);
@@ -173,19 +225,34 @@ int main(int argc, char *argv[])
 	cudaGetDeviceProperties(&device_properties, deviceOrdinal);
 	printf("%s\n\n", device_properties.name);
 
-	printf("Ucitavanje iz matrice: ");
+	printf("Generiranje matrice ");
 
 	CUT_SAFE_CALL(cutCreateTimer(&t));
 	CUT_SAFE_CALL(cutStartTimer(t));
 	
-	loadMatrix(m_in, "matrice/po1024.mat", size);
+	//loadMatrix(m_in, "matrice/po32.mat", size);
+	init(v, size * size);
+	standard(v, v, m_in, size);
 
 	CUT_SAFE_CALL(cutStopTimer(t));
 
 	printf("%f\n", cutGetTimerValue(t));
 
+	printf("CPU racuna ");
+
+	CUT_SAFE_CALL(cutCreateTimer(&ta));
+	CUT_SAFE_CALL(cutStartTimer(ta));
+	
+	//loadMatrix(m_in, "matrice/po32.mat", size);
+
+	cpu_dpotrf(m_in, cpu_rez, size);
+	CUT_SAFE_CALL(cutStopTimer(ta));
+
+	printf("%f\n", cutGetTimerValue(ta));
 	// GPU //
 	int n = size;
+	saveMatrix(m_in, "m.mat", n);
+	saveMatrix(cpu_rez, "cpu_rez.mat", n);
 	dim3 blokovaPoGridu, thredovaPoBloku;
 	
 	thredovaPoBloku.x = 16;
@@ -232,15 +299,11 @@ int main(int argc, char *argv[])
 				  ( device_m, size, 0 );
 
 	for (i = 0; i < n / 16 - 1; i++) {
-		cudaMemcpy( device_eye, 
-					eye, 
-					16*16*sizeof(float),
-					cudaMemcpyHostToDevice );
 		blokovaPoGridu.x = it;
 		blokovaPoGridu.y = it;
-		gpu_inv_l<<<1, 16>>>(device_m, device_eye, size, i);
-		gpu_mm_r<<<it, thredovaPoBloku>>>
-			(device_eye, device_m, size, i);
+		gpu_inv_l<<<it, 16>>>(device_m, size, i);
+//		gpu_mm_r<<<it, thredovaPoBloku>>>
+//			(device_eye, device_m, size, i);
 		/*if(it % 2){
 			blokovaPoGridu.y = (it+1)/2;
 			blokovaPoGridu.x = it;
@@ -297,21 +360,11 @@ int main(int argc, char *argv[])
 	
 	cudaMemcpy(m_out, device_m, 
 			n * n * sizeof(float), cudaMemcpyDeviceToHost);
-	
+	printf("CPU %.20lf\n", cpu_rez[(size-1)*size+size-1]);
+	printf("GPU %.20lf\n", m_out[(size-1)*size+size-1]);
 
 	saveMatrix(m_out, "rez.mat", n);
 
-	cudaMemcpy(eye, device_eye, 
-			16 * 16 *  sizeof(float), cudaMemcpyDeviceToHost);
-	
-
-	saveMatrix(eye, "oko.mat", 16);
-
-	cudaMemcpy(m_out, device_m_out, 
-			n * n * sizeof(float), cudaMemcpyDeviceToHost);
-	
-
-	saveMatrix(m_out, "out.mat", n);
 
 	free(m_in);
 	free(m_out);
